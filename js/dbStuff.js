73bba3fc9030ea659ecc14224a398f6561a666b8
jotasprout <jotasprout@gmail.com>
2018-09-16 21:34:44 -0400

    building pouchDB functions

--- js/dbStuff.js
+++ js/dbStuff.js
@@ -1,27 +1,39 @@
 (function () {
 
-    // const PouchDB = require('pouchdb');
-    // Do I need above if I have it linked locally?
+    const form = document.getElementById('structureInfo');
+    let structureObject;
 
-    const db = new PouchDB('structures');
+    const pdb = new PouchDB('structures');
 
     const remoteCouch = "http://admin:passWordsSuck!@localhost:5984/structures";
 
-    function makeStructure (date, time, hazards, living, dead, searchStatus) {
-        let structure = {
-            _id: new Date().toISOString(),
-            // look into collating as alternative to above
-            date: date,
-            time: time,
-            hazards: hazards,
-            living: living,
-            dead: dead,
-            searchCompleted: searchStatus
+    structureObject = function (event) {
+        let so = {};
+        if (event.target._id.value == '') {
+            so._id = new Date().toISOString();
+        } else {
+            so._id = event.target._id.value;
         };
-    }
+        // so.date = form.date.value;
+        // so.time = form.time.value;
+        so.hazards = form.hazards.value;
+        so.victims.dead = form.living.value;
+        so.victims.dead = form.dead.value;
+
+        pdb.put(so, function (error, response){
+            if (error) {
+                console.log(error);
+                return;
+            } else if (response && response.ok){
+                console.log("Successfully stored a structure");
+            }
+        });
+    } // end of structureObject
+
+    form.addEventListener('saveStructure', structureObject);
 
     function showStructures() {
-        db.allDocs (
+        pdb.allDocs (
             {
                 include_docs: true,
                 descending: true
@@ -32,7 +44,7 @@
         );
     }
 
-    db.changes({
+    pdb.changes({
         since: 'now',
         live: true
     }).on('change', showStructures);
@@ -40,8 +52,8 @@
     function sync(){
         syncDom.setAttribute('data-sync-state', 'syncing');
         const opts = {live: true};
-        db.replicate.to(remoteCouch, opts, synceError);
-        db.replicate.from(remoteCouch, opts, syncError);
+        pdb.replicate.to(remoteCouch, opts, synceError);
+        pdb.replicate.from(remoteCouch, opts, syncError);
     }
 
 })();
